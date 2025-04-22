51d1f2bd3cb6e666044f2ef6c84fb710ed368955
omnia <omniasaid060@gmail.com>
2025-04-22 15:07:51 +0200

    Initial commit

---
+++ app/src/main/java/com/example/solverapp/historyActivity.java
@@ -0,0 +1,35 @@
+package com.example.solverapp;
+
+import android.content.Intent;
+import android.os.Bundle;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class historyActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_history);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+    }
+
+    private void gotoHistoryActivity(){
+        Intent intent = new Intent(this, historyActivity.class);
+        startActivity(intent);
+    }
+
+    private void gotoResponseActivity(){
+        Intent intent = new Intent(this, responseActivity.class);
+        startActivity(intent);
+    }
+}
