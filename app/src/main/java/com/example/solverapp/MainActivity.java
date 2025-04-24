ee20514112071556ef930992b3cc11529be9b4a5
omnia <omniasaid060@gmail.com>
2025-04-24 14:50:46 +0200

    add istory activity

--- app/src/main/java/com/example/solverapp/MainActivity.java
+++ app/src/main/java/com/example/solverapp/MainActivity.java
@@ -28,7 +28,6 @@ public class MainActivity extends AppCompatActivity {
         historyBtn.setOnClickListener(v->{
             goToHistoryActivity();
         });
-
     }
 
     private void goToHistoryActivity(){
